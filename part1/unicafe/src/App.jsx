91b5bcf65268280058adc82461274c555fed39d7
Lajfa <dlajfa@hotmail.com>
2024-03-12 00:34:34 +0800

    add part1

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import Button from "./components/Button";
+import Statistics from "./components/Statistics";
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  const all = good + neutral + bad;
+  const average = (good - bad) / all;
+  const positive = `${(good * 100) / all}%`;
+
+  const incrementGood = () => {
+    setGood(good + 1);
+  };
+
+  const incrementNeutral = () => {
+    setNeutral(neutral + 1);
+  };
+
+  const incrementBad = () => {
+    setBad(bad + 1);
+  };
+
+  return (
+    <div>
+      <h2>give feedback</h2>
+      <Button label={"good"} onClick={incrementGood}></Button>
+      <Button label={"neutral"} onClick={incrementNeutral}></Button>
+      <Button label={"bad"} onClick={incrementBad}></Button>
+      <h2>statistics</h2>
+      <Statistics
+        good={good}
+        neutral={neutral}
+        bad={bad}
+        all={all}
+        average={average}
+        positive={positive}
+      />
+    </div>
+  );
+};
+
+export default App;
