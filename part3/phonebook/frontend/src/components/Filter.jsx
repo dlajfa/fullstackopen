ab737b7cfdfe15b159fefc5a55c09c54e44a616b
Lajfa <dlajfa@hotmail.com>
2024-03-15 18:36:04 +0800

    add part3

---
+++ part3/phonebook/frontend/src/components/Filter.jsx
@@ -0,0 +1,9 @@
+const Filter = ({ keyword, onChange }) => {
+  return (
+    <div>
+      filter shown with <input value={keyword} onChange={onChange} />
+    </div>
+  );
+};
+
+export default Filter;
